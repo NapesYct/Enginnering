ec8e42a9ba36780a3731df1a90105d4471d869f3
unknown <faleyecassiel005@gmail.com>
2022-11-18 04:41:17 -0800

    final

--- napes/pages/_app.tsx
+++ napes/pages/_app.tsx
@@ -2,23 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { AuthContextProvider } from '../context/AuthContext'
-import { useRouter } from 'next/router';
-import Protected from '../components/Protected';
+// import { useRouter } from 'next/router';
+// import Protected from '../components/Protected';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const notAuth = ["/"];
-  const router = useRouter()
   return (
     <AuthContextProvider>
       <Layout>
-        {notAuth.includes(router.pathname) ? <Component {...pageProps} /> :
-
-          <Protected>
-            <Component {...pageProps} />
-          </Protected>
-
-        }
+        <Component {...pageProps} />
 
       </Layout>
     </AuthContextProvider>
